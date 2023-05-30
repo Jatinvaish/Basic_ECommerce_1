a35b81f3a9f996c6d542a1176c91e0f52fc6a06b
Jatinvaish <1195jatinvaishnav@gmail.com>
2023-05-30 21:37:32 +0530

    bug -- solve live api issue

--- backend/app.js
+++ backend/app.js
@@ -39,11 +39,11 @@ app.use("/api/v1", payment);
 
 
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 
 // ================================================================
