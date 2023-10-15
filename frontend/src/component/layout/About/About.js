b950f1857b545f9741ecdc7f27cc6211817baf14
Jatinvaish <1195jatinvaishnav@gmail.com>
2023-10-15 10:47:03 +0530

    changes name jatin

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -5,7 +5,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://www.instagram.com/manefest.co/?igshid=MzRlODBiNWFlZA%3D%3D/";
+    window.location = "https://www.instagram.com/jatin_vaishnav__2003/";
   };
   return (
     <div className="aboutSection">
@@ -21,25 +21,21 @@ const About = () => {
               src=""
               alt="Founder"
             />
-            <Typography>Shakir</Typography>
+            <Typography>Jatin</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @Shakir
+              This is a sample wesbite made by @Jatin
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
-            {/* <a
+            <a
               href="https://www.linkedin.com/in/jatin-vaishnav-aa5427220/"
               target="blank"
             >
               <LinkedInIcon className="youtubeSvgIcon" />
-            </a> */}
-
-            <a href="https://www.instagram.com/manefest.co/?igshid=MzRlODBiNWFlZA%3D%3D" target="blank">
-              <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
         </div>
